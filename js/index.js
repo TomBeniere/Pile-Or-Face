78e633d5f804e1e4a510d47396b4267bb44d6e91
Fenidorn <tbeniere@edenschool.fr>
2019-10-02 17:09:39 +0200

    ajustement des taille mobile , tablet , desktop + scoreboard en desktop

--- js/index.js
+++ js/index.js
@@ -6,6 +6,8 @@ var play = document.getElementById("play");
 var bitcoin = document.getElementById("bitcoin");
 var goldcoin = document.getElementById("goldcoin");
 var main = document.getElementById("main");
+var score = document.getElementById("score");
+var point = 0;
 
 butbit.addEventListener("click", Bit);
 butgold.addEventListener("click", Gold);
@@ -30,41 +32,38 @@ function Gold() {
 
 function animation() {
     main.style.position = "relative";
-    
+
     bitcoin.style.zIndex = "100";
     goldcoin.style.zIndex = "50";
     bitcoin.style.position = "absolute";
     goldcoin.style.position = "absolute";
-    goldcoin.style.transform = "rotateY(-180deg)";    
+    goldcoin.style.transform = "rotateY(-180deg)";
     bitcoin.style.backfaceVisibility = "hidden";
     goldcoin.style.backfaceVisibility = "hidden";
 
-    setTimeout(function() {
+    setTimeout(function () {
         if (resultat == "Bitcoin") {
-        bitcoin.style.transform = "rotateY(2520deg)";
-        goldcoin.style.transform = "rotateY(1620deg)";
-        bitcoin.style.transition = "2s";
-        goldcoin.style.transition = "2s";
-    } else {
-        bitcoin.style.transform = "rotateY(2700deg)";
-        goldcoin.style.transform = "rotateY(1800deg)";
-        bitcoin.style.transition = "2s";
-        goldcoin.style.transition = "2s";
-    }
+            bitcoin.style.transform = "rotateY(2520deg)";
+            goldcoin.style.transform = "rotateY(1620deg)";
+            bitcoin.style.transition = "2s";
+            goldcoin.style.transition = "2s";
+        } else {
+            bitcoin.style.transform = "rotateY(2700deg)";
+            goldcoin.style.transform = "rotateY(1800deg)";
+            bitcoin.style.transition = "2s";
+            goldcoin.style.transition = "2s";
+        }
     }, 500)
-    
-    
-    
+
+    setTimeout(function () {
+            if (choose == resultat) {
+                point += 1;
+                score.innerHTML = "Score : " + point;
+            }
+    }, 2500)
 }
 
 function BitOrGold() {
     aleatoire();
     animation();
-    console.log("mon choix " + choose);
-    console.log("la reponse " + resultat);
-    if (choose == resultat) {
-        console.log("bien joué c'était " + resultat);
-    } else {
-        console.log("mal joué c'etait " + resultat);
-    }
 }
