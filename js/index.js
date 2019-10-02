40d2158e4dc508b6e1771617e05fc5789e29a94c
Fenidorn <tbeniere@edenschool.fr>
2019-10-02 16:32:08 +0200

    Ajout animation 3d js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
 var choose = "";
 var resultat = "";
+var butbit = document.getElementById("butbitcoin");
+var butgold = document.getElementById("butgoldcoin");
+var play = document.getElementById("play");
 var bitcoin = document.getElementById("bitcoin");
 var goldcoin = document.getElementById("goldcoin");
+var main = document.getElementById("main");
+
+butbit.addEventListener("click", Bit);
+butgold.addEventListener("click", Gold);
+play.addEventListener("click", BitOrGold);
+
 
 function aleatoire() {
     if (Math.floor(Math.random() * (2 - 0) + 0) >= 1) {
@@ -19,12 +28,43 @@ function Gold() {
     return choose = "Goldcoin";
 }
 
-function BitOrGold() {
-    aleatoire();
-    if (choose == resultat) {
-        alert("bien joué c'était " + resultat);
+function animation() {
+    main.style.position = "relative";
+    
+    bitcoin.style.zIndex = "100";
+    goldcoin.style.zIndex = "50";
+    bitcoin.style.position = "absolute";
+    goldcoin.style.position = "absolute";
+    goldcoin.style.transform = "rotateY(-180deg)";    
+    bitcoin.style.backfaceVisibility = "hidden";
+    goldcoin.style.backfaceVisibility = "hidden";
+
+    setTimeout(function() {
+        if (resultat == "Bitcoin") {
+        bitcoin.style.transform = "rotateY(360deg)";
+        goldcoin.style.transform = "rotateY(180deg)";
+        bitcoin.style.transition = "3s";
+        goldcoin.style.transition = "3s";
     } else {
-        alert("mal joué c'etait " + resultat);
+        bitcoin.style.transform = "rotateY(540deg)";
+        goldcoin.style.transform = "rotateY(360deg)";
+        bitcoin.style.transition = "3s";
+        goldcoin.style.transition = "3s";
     }
+    }, 500)
+    
+    
+    
 }
 
+function BitOrGold() {
+    aleatoire();
+    animation();
+    console.log("mon choix " + choose);
+    console.log("la reponse " + resultat);
+    if (choose == resultat) {
+        console.log("bien joué c'était " + resultat);
+    } else {
+        console.log("mal joué c'etait " + resultat);
+    }
+}
